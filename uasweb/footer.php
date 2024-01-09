88f057d4b5250b725d417ba107e64e2879be9e8d
adeliadhea06 <115794875+adeliadhea06@users.noreply.github.com>
2024-01-09 07:49:37 +0700

    Delete uasweb directory

--- uasweb/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <strong>UAS WEB &copy; KELOMPOK 5 TI.22.A1</strong>
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Teknik Informatika,</b> Universitas Pelita Bangsa
-    </div>
-  </footer>
